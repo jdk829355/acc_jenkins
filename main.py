84c57d93166c5977112aeb3aa652f4e77eedce25
jdk829355 <jdk829355@gmail.com>
2026-03-27 20:05:46 +0900

    sdfas

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 def main():
-    print("Hello from acc-jenkins! this is a test for jenkins pipeline.")
+    print("Hello from acc-jenkins! this is for jenkins pipeline.")
 
 
 if __name__ == "__main__":
